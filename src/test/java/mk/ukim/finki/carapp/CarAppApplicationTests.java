95236e0977d697fe939b2d8be07e57c959867488
srbinovskia46 <andrej.srbinovski@students.finki.ukim.mk>
2024-06-21 23:32:16 +0200

    Edited Dockerfile

--- src/test/java/mk/ukim/finki/carapp/CarAppApplicationTests.java
+++ src/test/java/mk/ukim/finki/carapp/CarAppApplicationTests.java
@@ -1,13 +1,13 @@
-package mk.ukim.finki.carapp;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class CarAppApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
-
-}
+//package mk.ukim.finki.carapp;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class CarAppApplicationTests {
+//
+//    @Test
+//    void contextLoads() {
+//    }
+//
+//}
